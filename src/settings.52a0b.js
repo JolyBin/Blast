c9a450fd92c56d6a51848f6ef0cafb124e0e3520
Joly_Bin <vladavenir@mail.ru>
2026-02-09 16:21:30 +1000

    test build

---
+++ src/settings.52a0b.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/helloworld.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",resources:"17c81",main:"6e77f"}};
