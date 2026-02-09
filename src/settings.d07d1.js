4c646857931fa29b48281e6b5528b26058354470
Joly_Bin <vladavenir@mail.ru>
2026-02-09 17:48:52 +1000

    add load animation

--- src/settings.d07d1.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Main Scene.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"17c81",main:"45fb3"}};
