5990904b785e7ffb02837cf775aa4bfc971c4a04
Joly_Bin <vladavenir@mail.ru>
2026-02-09 22:34:35 +1000

    beta release

---
+++ src/settings.713ae.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Main Scene.fire",orientation:"",jsList:[],bundleVers:{internal:"52d36",resources:"e8ef1",main:"03351"}};
